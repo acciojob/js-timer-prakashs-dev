3de91ecf4ffdaabdd7a411477b18b9414764319c
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-12-12 12:31:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 // write js code here if required
+const t = new Date();
+const timer = document.getElementById("timer").innerText = `${t.getDay()}/${t.getMonth()}/${t.getFullYear()}, ${t.getHours()}:${t.getMinutes()}:${t.getSeconds()}`;
